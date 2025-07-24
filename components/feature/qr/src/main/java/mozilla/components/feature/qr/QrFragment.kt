3933f7ef07a469492360c39eb6b2a4bef22f68e5
Harrison Oglesby <oglesby.harrison@gmail.com>
2025-07-24 17:58:08 +0000

    Bug 1948761 - Part 4: fix QrFragment back handling for API 33+ r=android-reviewers,petru
    Differential Revision: https://phabricator.services.mozilla.com/D256719

--- components/feature/qr/src/main/java/mozilla/components/feature/qr/QrFragment.kt
+++ components/feature/qr/src/main/java/mozilla/components/feature/qr/QrFragment.kt
@@ -71,6 +71,7 @@ import mozilla.components.feature.qr.views.CustomViewFinder
 import mozilla.components.support.base.log.logger.Logger
 import mozilla.components.support.ktx.android.content.hasCamera
 import mozilla.components.support.ktx.android.content.isPermissionGranted
+import mozilla.components.support.utils.ext.handleBackEvents
 import java.io.Serializable
 import java.util.Collections
 import java.util.concurrent.Executor
@@ -184,6 +185,11 @@ class QrFragment : Fragment() {
     }
 
     /**
+     * [Surface] used in the camera.
+     */
+    private var surface: Surface? = null
+
+    /**
      * An additional thread for running tasks that shouldn't block the UI.
      * A [Handler] for running tasks in the background.
      */
@@ -267,14 +273,20 @@ class QrFragment : Fragment() {
 
         CustomViewFinder.setMessage(scanMessage)
         qrState = STATE_FIND_QRCODE
+
+        val root = getView()
+        root?.isFocusableInTouchMode = true
+        root?.requestFocus()
+
+        view.handleBackEvents {
+            stopServices()
+            scanCompleteListener?.onScanComplete("")
+        }
     }
 
     override fun onResume() {
         super.onResume()
 
-        val root = requireView()
-        root.isFocusableInTouchMode = true
-        root.requestFocus()
         // It's possible that the Fragment is resumed to a scanning state
         // while in the meantime the camera permission was removed. Avoid any issues.
         if (requireContext().isPermissionGranted(permission.CAMERA)) {
@@ -288,9 +300,7 @@ class QrFragment : Fragment() {
     }
 
     override fun onPause() {
-        closeCamera()
-        stopBackgroundThread()
-        stopExecutorService()
+        stopServices()
         super.onPause()
     }
 
@@ -299,6 +309,12 @@ class QrFragment : Fragment() {
         qrState = STATE_FIND_QRCODE
 
         super.onStop()
+    }
+
+    private fun stopServices() {
+        closeCamera()
+        stopBackgroundThread()
+        stopExecutorService()
     }
 
     internal fun maybeStartBackgroundThread() {
@@ -505,6 +521,8 @@ class QrFragment : Fragment() {
             imageReader?.close()
             imageReader = null
 
+            surface?.release()
+
             // captureSession should be closed as a last step in case background executor terminated
             captureSession?.close()
             captureSession = null
@@ -561,16 +579,21 @@ class QrFragment : Fragment() {
         // We configure the size of default buffer to be the size of camera preview we want.
         texture?.setDefaultBufferSize(size.width, size.height)
 
-        val surface = Surface(texture)
+        surface = Surface(texture)
 
         // If image reader's surface is null, stop here.
         val imageSurface = imageReader?.surface ?: return
+
+        if (surface == null) {
+            scanCompleteListener?.onScanComplete("")
+            return
+        }
 
         handleCaptureException("Failed to create camera preview session") {
             cameraDevice?.let {
                 previewRequestBuilder = it.createCaptureRequest(CameraDevice.TEMPLATE_PREVIEW).apply {
                     addTarget(imageSurface)
-                    addTarget(surface)
+                    addTarget(surface!!)
                 }
 
                 val captureCallback = object : CameraCaptureSession.CaptureCallback() {}
@@ -608,7 +631,7 @@ class QrFragment : Fragment() {
                         logger.error("Failed to configure CameraCaptureSession")
                     }
                 }
-                createCaptureSessionCompat(it, imageSurface, surface, stateCallback)
+                createCaptureSessionCompat(it, imageSurface, surface!!, stateCallback)
             }
         }
     }
