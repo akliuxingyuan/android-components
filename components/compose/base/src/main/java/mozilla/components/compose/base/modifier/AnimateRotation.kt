8ddf42463263671c3be88468f3491438e6ad7581
Titouan Thibaud <tthibaud@mozilla.com>
2025-09-30 09:43:43 +0000

    Bug 1991434 - Use Android Components animateRotation on Translation's DownloadIndicator. r=android-reviewers,007
    Differential Revision: https://phabricator.services.mozilla.com/D266675

--- components/compose/base/src/main/java/mozilla/components/compose/base/modifier/AnimateRotation.kt
+++ components/compose/base/src/main/java/mozilla/components/compose/base/modifier/AnimateRotation.kt
@@ -6,6 +6,8 @@ package mozilla.components.compose.base.modifier
 
 import androidx.compose.animation.core.Animatable
 import androidx.compose.animation.core.LinearEasing
+import androidx.compose.animation.core.RepeatMode
+import androidx.compose.animation.core.infiniteRepeatable
 import androidx.compose.animation.core.tween
 import androidx.compose.material3.Button
 import androidx.compose.material3.Icon
@@ -14,15 +16,14 @@ import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.rememberUpdatedState
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.composed
 import androidx.compose.ui.draw.rotate
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
-import kotlinx.coroutines.isActive
 import mozilla.components.compose.base.theme.AcornTheme
+import kotlin.math.ceil
 import mozilla.components.ui.icons.R as iconsR
 
 /**
@@ -47,39 +48,32 @@ fun Modifier.animateRotation(
     durationMillis: Int = DEFAULT_ANIMATION_DURATION_MS,
 ) = composed {
     val rotation = remember { Animatable(NO_ROTATION) }
-    val latestIsAnimating by rememberUpdatedState(animate)
 
     LaunchedEffect(animate) {
-        if (latestIsAnimating) {
-            while (isActive) {
-                rotation.animateTo(
-                    targetValue = FULL_ROTATION,
-                    animationSpec = tween(
-                        durationMillis = durationMillis,
-                        easing = LinearEasing,
-                    ),
-                )
-                rotation.snapTo(0f)
-            }
+        if (animate) {
+            rotation.animateTo(
+                targetValue = rotation.value + FULL_ROTATION,
+                animationSpec = infiniteRepeatable(
+                    animation = tween(durationMillis, easing = LinearEasing),
+                    repeatMode = RepeatMode.Restart,
+                ),
+            )
         } else {
-            val nearestTarget = when {
-                rotation.value == NO_ROTATION -> NO_ROTATION
-                rotation.value <= HALF_ROTATION -> HALF_ROTATION
-                else -> FULL_ROTATION
-            }
-            val leftover = nearestTarget - rotation.value
+            // When stopping the animation, we want to smoothly animate to the nearest 180-degree angle.
+            val currentRotation = rotation.value
+            val targetRotation = ceil(currentRotation / HALF_ROTATION) * HALF_ROTATION
+            val leftover = targetRotation - currentRotation
+
             if (leftover > 0f) {
                 val remainingTime = leftover * durationMillis / FULL_ROTATION
                 rotation.animateTo(
-                    targetValue = rotation.value + leftover,
+                    targetValue = targetRotation,
                     animationSpec = tween(
                         durationMillis = remainingTime.toInt(),
                         easing = LinearEasing,
                     ),
                 )
             }
-
-            rotation.snapTo(NO_ROTATION)
         }
     }
 
