d68c65e3f6e81f92b0c1207a70b234b5e6f7c284
mcarare <48995920+mcarare@users.noreply.github.com>
2025-11-25 08:45:15 +0000

    Bug 2000856 - Remove MainCoroutineRule from FxaWebChannelFeatureTest. r=android-reviewers,avirvara
    Differential Revision: https://phabricator.services.mozilla.com/D273948

--- components/feature/accounts/src/test/java/mozilla/components/feature/accounts/FxaWebChannelFeatureTest.kt
+++ components/feature/accounts/src/test/java/mozilla/components/feature/accounts/FxaWebChannelFeatureTest.kt
@@ -28,7 +28,6 @@ import mozilla.components.support.test.any
 import mozilla.components.support.test.argumentCaptor
 import mozilla.components.support.test.eq
 import mozilla.components.support.test.mock
-import mozilla.components.support.test.rule.MainCoroutineRule
 import mozilla.components.support.test.whenever
 import mozilla.components.support.webextensions.BuiltInWebExtensionController
 import org.json.JSONException
@@ -38,7 +37,6 @@ import org.junit.Assert.assertFalse
 import org.junit.Assert.assertNull
 import org.junit.Assert.assertTrue
 import org.junit.Before
-import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.mockito.Mockito.clearInvocations
@@ -50,9 +48,6 @@ import org.robolectric.Shadows.shadowOf
 
 @RunWith(AndroidJUnit4::class)
 class FxaWebChannelFeatureTest {
-
-    @get:Rule
-    val coroutinesTestRule = MainCoroutineRule()
 
     @Before
     fun setup() {
