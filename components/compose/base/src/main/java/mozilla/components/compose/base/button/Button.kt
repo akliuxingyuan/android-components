f41d3774f1d0358c4a8d4652d82aa51d98475782
Serban Stanca <sstanca@mozilla.com>
2025-12-02 15:40:13 +0200

    Revert "Bug 1993368, Bug 2003337, Bug 1993856 - Part 40: Migrate ImagesPlaceholder to use M3 color tokens r=android-reviewers,007" as requested by aaronmt for causing CreditCardAutofill related fenix-debug failures.
    This reverts commit 52fdd186700f329256cc2fd782ad6a48d0549fdb.
    
    This reverts commit c34845216f2d5450ae92b73750775d5c3b77864a.
    
    This reverts commit f5de84c293fd05b61d9d26eac45bdab8fbe05be8.
    
    This reverts commit 9d5ec4665d21c6cee93f48db44d1a397affbc07c.
    
    This reverts commit a2e4ac9ecd70d47a7fcf2ba3911135867397c883.
    
    This reverts commit ddcf5ec0787055978e20178a6ad4a92f4903cea5.
    
    This reverts commit fc351100eafea724a49c0877100e07bd8de2fd31.

--- components/compose/base/src/main/java/mozilla/components/compose/base/button/Button.kt
+++ components/compose/base/src/main/java/mozilla/components/compose/base/button/Button.kt
@@ -5,6 +5,7 @@
 package mozilla.components.compose.base.button
 
 import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
@@ -13,10 +14,8 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.ButtonDefaults.outlinedButtonBorder
-import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.ReadOnlyComposable
@@ -26,12 +25,9 @@ import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.tooling.preview.PreviewLightDark
 import androidx.compose.ui.unit.dp
 import mozilla.components.compose.base.theme.AcornTheme
-import mozilla.components.compose.base.theme.acornPrivateColorScheme
-import mozilla.components.compose.base.theme.privateColorPalette
 import androidx.compose.material3.Button as M3Button
 import androidx.compose.material3.OutlinedButton as M3OutlinedButton
 import mozilla.components.ui.icons.R as iconsR
@@ -63,7 +59,6 @@ private fun ButtonContent(
         )
         Spacer(modifier = Modifier.width(AcornTheme.layout.space.static100))
     }
-
     Text(
         text = text,
         textAlign = TextAlign.Center,
@@ -107,40 +102,6 @@ fun FilledButton(
         ),
     ) {
         ButtonContent(text = text, icon = icon, modifier = iconModifier)
-    }
-}
-
-/**
- * Filled button.
- *
- * @param onClick Invoked when the user clicks on the button.
- * @param modifier [Modifier] to be applied to the layout.
- * @param enabled Controls the enabled state of the button.
- * When false, this button will not be clickable.
- * @param contentColor The color to be used for the button's text and icon when enabled.
- * @param containerColor The background color of the button when enabled.
- * @param content [Composable] content to be displayed in the button.
- */
-@Composable
-fun FilledButton(
-    onClick: () -> Unit,
-    modifier: Modifier = Modifier,
-    enabled: Boolean = true,
-    contentColor: Color = ButtonDefaults.buttonColors().contentColor,
-    containerColor: Color = ButtonDefaults.buttonColors().containerColor,
-    content: @Composable () -> Unit,
-) {
-    M3Button(
-        onClick = onClick,
-        modifier = modifier,
-        enabled = enabled,
-        contentPadding = AcornTheme.buttonContentPadding(),
-        colors = ButtonDefaults.buttonColors(
-            containerColor = containerColor,
-            contentColor = contentColor,
-        ),
-    ) {
-        content()
     }
 }
 
@@ -230,19 +191,17 @@ fun DestructiveButton(
 }
 
 @Composable
-private fun ButtonPreviewContent() {
-    Surface {
+@PreviewLightDark
+private fun ButtonPreview() {
+    AcornTheme {
         Column(
-            modifier = Modifier.padding(16.dp),
+            modifier = Modifier
+                .background(MaterialTheme.colorScheme.surface)
+                .padding(16.dp),
             verticalArrangement = Arrangement.spacedBy(16.dp),
         ) {
             FilledButton(
                 text = "Label",
-                onClick = {},
-            )
-
-            FilledButton(
-                text = "Label",
                 icon = painterResource(iconsR.drawable.mozac_ic_collection_24),
                 onClick = {},
             )
@@ -251,17 +210,6 @@ private fun ButtonPreviewContent() {
                 text = "Label",
                 enabled = false,
                 icon = painterResource(iconsR.drawable.mozac_ic_collection_24),
-                onClick = {},
-            )
-
-            FilledButton(
-                onClick = {},
-            ) {
-                CircularProgressIndicator(color = MaterialTheme.colorScheme.onPrimary)
-            }
-
-            OutlinedButton(
-                text = "Label",
                 onClick = {},
             )
 
@@ -275,11 +223,6 @@ private fun ButtonPreviewContent() {
                 text = "Label",
                 enabled = false,
                 icon = painterResource(iconsR.drawable.mozac_ic_collection_24),
-                onClick = {},
-            )
-
-            DestructiveButton(
-                text = "Label",
                 onClick = {},
             )
 
@@ -296,24 +239,5 @@ private fun ButtonPreviewContent() {
                 onClick = {},
             )
         }
-    }
-}
-
-@Composable
-@PreviewLightDark
-private fun ButtonPreview() {
-    AcornTheme {
-        ButtonPreviewContent()
-    }
-}
-
-@Composable
-@Preview
-private fun ButtonPrivatePreview() {
-    AcornTheme(
-        colors = privateColorPalette,
-        colorScheme = acornPrivateColorScheme(),
-    ) {
-        ButtonPreviewContent()
     }
 }
