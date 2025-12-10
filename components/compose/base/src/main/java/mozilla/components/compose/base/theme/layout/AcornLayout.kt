30b401496abbf51c2ccb2785b3d644a4ee8212d5
Gabriel Luong <gabriel.luong@gmail.com>
2025-12-10 15:33:06 +0000

    Bug 2004511 - Part 5: Replace AcornTheme usage with FirefoxTheme in Fenix r=android-reviewers,007
    Differential Revision: https://phabricator.services.mozilla.com/D275382

--- components/compose/base/src/main/java/mozilla/components/compose/base/theme/layout/AcornLayout.kt
+++ components/compose/base/src/main/java/mozilla/components/compose/base/theme/layout/AcornLayout.kt
@@ -11,7 +11,6 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.FlowRow
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
