d5743b51cf998211c3ba8df43cffc09b660413b4
iulian moraru <imoraru@mozilla.com>
2026-02-11 14:13:34 +0200

    Revert "Bug 2015074 - Part 2: Add a detekt rule to encourage `DateTimeProvider` usage instead of direct access. r=android-reviewers,gmalekpour" for causing multiple fenix failures.
    This reverts commit 954a48293b449a719d3453133e08fe036683fc98.
    
    Revert "Bug 2015074 - Part 1: Moved the DateTimeProvider from the downloads feature component to the support utils component. r=android-reviewers,nalexander"
    
    This reverts commit 30c105a4648a7c15fc8f472e0392251bd43b983b.

--- components/feature/downloads/src/main/java/mozilla/components/feature/downloads/DateTimeProvider.kt
+++ components/feature/downloads/src/main/java/mozilla/components/feature/downloads/DateTimeProvider.kt
@@ -2,7 +2,7 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-package mozilla.components.support.utils
+package mozilla.components.feature.downloads
 
 import java.time.LocalDate
 import java.time.ZoneId
