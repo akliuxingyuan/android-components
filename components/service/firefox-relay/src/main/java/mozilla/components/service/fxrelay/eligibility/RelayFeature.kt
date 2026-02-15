9aba77ff640197a20bbb2330fa52a673d77c7eac
Jonathan Almeida <jonalmeida942@gmail.com>
2026-02-15 19:57:44 +0000

    Bug 1996722 - Part 3: Show info messages on email mask edge cases r=gmalekpour,android-reviewers,android-l10n-reviewers,flod
    Differential Revision: https://phabricator.services.mozilla.com/D282564

--- components/service/firefox-relay/src/main/java/mozilla/components/service/fxrelay/eligibility/RelayFeature.kt
+++ components/service/firefox-relay/src/main/java/mozilla/components/service/fxrelay/eligibility/RelayFeature.kt
@@ -108,7 +108,9 @@ class RelayFeature(
      * @return an email masks or `null` if the operation fails.
      */
     suspend fun getOrCreateNewMask(generatedFor: String): EmailMask? {
-        return fxRelay?.createEmailMask(generatedFor)
+        val mask = fxRelay?.createEmailMask(generatedFor)
+        store.dispatch(RelayEligibilityAction.UpdateLastUsed(mask))
+        return mask
     }
 
     private inner class RelayAccountObserver : AccountObserver {
