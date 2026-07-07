ee4bd601167cd79c18d7c57d98610276939e54de
Gabriel Luong <gabriel.luong@gmail.com>
2026-07-07 14:31:11 +0000

    Bug 2052681 - Part 3: Remove the legacy Pocket stories database r=android-reviewers,petru
    - Remove the `pocket_recommendations` Room database.
    - Delete PocketRecommendationsDatabase, its DAO and the PocketStoryEntity, the spocs entities, the exported schemas, the remaining PocketApiStory type, and the associated tests.
    
    Differential Revision: https://phabricator.services.mozilla.com/D310562

--- components/service/pocket/src/test/java/mozilla/components/service/pocket/helpers/PocketTestResources.kt
+++ components/service/pocket/src/test/java/mozilla/components/service/pocket/helpers/PocketTestResources.kt
@@ -5,7 +5,6 @@
 package mozilla.components.service.pocket.helpers
 
 import mozilla.components.service.pocket.PocketStory.ContentRecommendation
-import mozilla.components.service.pocket.PocketStory.PocketRecommendedStory
 import mozilla.components.service.pocket.PocketStory.SponsoredContent
 import mozilla.components.service.pocket.PocketStory.SponsoredContentCallbacks
 import mozilla.components.service.pocket.PocketStory.SponsoredContentFrequencyCaps
@@ -18,9 +17,6 @@ import mozilla.components.service.pocket.mars.db.SponsoredContentEntity
 import mozilla.components.service.pocket.recommendations.api.ContentRecommendationResponseItem
 import mozilla.components.service.pocket.recommendations.api.ContentRecommendationsResponse
 import mozilla.components.service.pocket.recommendations.db.ContentRecommendationEntity
-import mozilla.components.service.pocket.spocs.db.SpocEntity
-import mozilla.components.service.pocket.stories.api.PocketApiStory
-import mozilla.components.service.pocket.stories.db.PocketStoryEntity
 
 private const val POCKET_DIR = "pocket"
 
@@ -28,26 +24,6 @@ private const val POCKET_DIR = "pocket"
  * Accessors to resources used in testing.
  */
 internal object PocketTestResources {
-    val pocketEndpointFiveStoriesResponse = this::class.java.classLoader!!.getResource(
-        "$POCKET_DIR/stories_recommendations_response.json",
-    )!!.readText()
-
-    val pocketEndpointThreeSpocsResponse = this::class.java.classLoader!!.getResource(
-        "$POCKET_DIR/sponsored_stories_response.json",
-    )!!.readText()
-
-    val pocketEndpointNullTitleStoryBadResponse = this::class.java.classLoader!!.getResource(
-        "$POCKET_DIR/story_recommendation_null_title_response.json",
-    )!!.readText()
-
-    val pocketEndpointNullUrlStoryBadResponse = this::class.java.classLoader!!.getResource(
-        "$POCKET_DIR/story_recommendation_null_url_response.json",
-    )!!.readText()
-
-    val pocketEndpointNullImageUrlStoryBadResponse = this::class.java.classLoader!!.getResource(
-        "$POCKET_DIR/story_recommendation_null_imageUrl_response.json",
-    )!!.readText()
-
     val contentRecommendationsJSONResponse = this::class.java.classLoader!!.getResource(
         "$POCKET_DIR/content_recommendations_response.json",
     )!!.readText()
@@ -63,83 +39,6 @@ internal object PocketTestResources {
     val marsSponsoredStoriesNullUrlResponse = this::class.java.classLoader!!.getResource(
         "$POCKET_DIR/mars_sponsored_stories_null_url_response.json",
     )!!.readText()
-
-    val apiExpectedPocketStoriesRecommendations: List<PocketApiStory> = listOf(
-        PocketApiStory(
-            title = "How to Remember Anything You Really Want to Remember, Backed by Science",
-            url = "https://getpocket.com/explore/item/how-to-remember-anything-you-really-want-to-remember-backed-by-science",
-            imageUrl = "https://img-getpocket.cdn.mozilla.net/{wh}/filters:format(jpeg):quality(60):no_upscale():strip_exif()/https%3A%2F%2Fpocket-image-cache.com%2F1200x%2Ffilters%3Aformat(jpg)%3Aextract_focal()%2Fhttps%253A%252F%252Fwww.incimages.com%252Fuploaded_files%252Fimage%252F1920x1080%252Fgetty-862457080_394628.jpg",
-            publisher = "Pocket",
-            category = "general",
-            timeToRead = 3,
-        ),
-        PocketApiStory(
-            title = "‘I Don’t Want to Be Like a Family With My Co-Workers’",
-            url = "https://www.thecut.com/article/i-dont-want-to-be-like-a-family-with-my-co-workers.html",
-            imageUrl = "https://img-getpocket.cdn.mozilla.net/{wh}/filters:format(jpeg):quality(60):no_upscale():strip_exif()/https%3A%2F%2Fpyxis.nymag.com%2Fv1%2Fimgs%2Fac8%2Fd22%2F315cd0cf1e3a43edfe0e0548f2edbcb1a1-ask-a-boss.1x.rsocial.w1200.jpg",
-            publisher = "The Cut",
-            category = "general",
-            timeToRead = 5,
-        ),
-        PocketApiStory(
-            title = "How America Failed in Afghanistan",
-            url = "https://www.newyorker.com/news/q-and-a/how-america-failed-in-afghanistan",
-            imageUrl = "https://img-getpocket.cdn.mozilla.net/{wh}/filters:format(jpeg):quality(60):no_upscale():strip_exif()/https%3A%2F%2Fmedia.newyorker.com%2Fphotos%2F6119484157b611aec9c99b43%2F16%3A9%2Fw_1280%2Cc_limit%2FChotiner-Afghanistan01.jpg",
-            publisher = "The New Yorker",
-            category = "general",
-            timeToRead = 14,
-        ),
-        PocketApiStory(
-            title = "How digital beauty filters perpetuate colorism",
-            url = "https://www.technologyreview.com/2021/08/15/1031804/digital-beauty-filters-photoshop-photo-editing-colorism-racism/",
-            imageUrl = "https://img-getpocket.cdn.mozilla.net/{wh}/filters:format(jpeg):quality(60):no_upscale():strip_exif()/https%3A%2F%2Fwp.technologyreview.com%2Fwp-content%2Fuploads%2F2021%2F08%2FBeautyScoreColorism.jpg%3Fresize%3D1200%2C600",
-            publisher = "MIT Technology Review",
-            category = "general",
-            timeToRead = 11,
-        ),
-        PocketApiStory(
-            title = "How to Get Rid of Black Mold Naturally",
-            url = "https://getpocket.com/explore/item/how-to-get-rid-of-black-mold-naturally",
-            imageUrl = "https://img-getpocket.cdn.mozilla.net/{wh}/filters:format(jpeg):quality(60):no_upscale():strip_exif()/https%3A%2F%2Fpocket-image-cache.com%2F1200x%2Ffilters%3Aformat(jpg)%3Aextract_focal()%2Fhttps%253A%252F%252Fpocket-syndicated-images.s3.amazonaws.com%252Farticles%252F6757%252F1628024495_6109ae86db6cc.png",
-            publisher = "Pocket",
-            category = "general",
-            timeToRead = 4,
-        ),
-    )
-
-    val dbExpectedPocketStory = PocketStoryEntity(
-        title = "How to Get Rid of Black Mold Naturally",
-        url = "https://getpocket.com/explore/item/how-to-get-rid-of-black-mold-naturally",
-        imageUrl = "https://img-getpocket.cdn.mozilla.net/{wh}/filters:format(jpeg):quality(60):no_upscale():strip_exif()/https%3A%2F%2Fpocket-image-cache.com%2F1200x%2Ffilters%3Aformat(jpg)%3Aextract_focal()%2Fhttps%253A%252F%252Fpocket-syndicated-images.s3.amazonaws.com%252Farticles%252F6757%252F1628024495_6109ae86db6cc.png",
-        publisher = "Pocket",
-        category = "general",
-        timeToRead = 4,
-        timesShown = 23,
-    )
-
-    val clientExpectedPocketStory = PocketRecommendedStory(
-        title = "How digital beauty filters perpetuate colorism",
-        url = "https://www.technologyreview.com/2021/08/15/1031804/digital-beauty-filters-photoshop-photo-editing-colorism-racism/",
-        imageUrl = "https://img-getpocket.cdn.mozilla.net/{wh}/filters:format(jpeg):quality(60):no_upscale():strip_exif()/https%3A%2F%2Fwp.technologyreview.com%2Fwp-content%2Fuploads%2F2021%2F08%2FBeautyScoreColorism.jpg%3Fresize%3D1200%2C600",
-        publisher = "MIT Technology Review",
-        category = "general",
-        timeToRead = 11,
-        timesShown = 3,
-    )
-
-    val dbExpectedPocketSpoc = SpocEntity(
-        id = 193815086,
-        url = "https://i.geistm.com/l/GC_7ReasonsKetoV2_Journiest?bcid=601c567ac5b18a0414cce1d4&bhid=624f3ea9adad7604086ac6b3&utm_content=PKT_A_7ReasonsKetoV2_Journiest_40702022_RawMeatballUGC_130Off_601c567ac5b18a0414cce1d4_624f3ea9adad7604086ac6b3&tv=su4&ct=NAT-PK-PROS-130OFF5WEEK-037&utm_medium=DB&utm_source=pocket~geistm&utm_campaign=PKT_A_7ReasonsKetoV2_Journiest_40702022_RawMeatballUGC_130Off",
-        title = "Eating Keto Has Never Been So Easy With Green Chef",
-        imageUrl = "https://img-getpocket.cdn.mozilla.net/direct?url=realUrl.png&resize=w618-h310",
-        sponsor = "Green Chef",
-        clickShim = "193815086ClickShim",
-        impressionShim = "193815086ImpressionShim",
-        priority = 3,
-        lifetimeCapCount = 50,
-        flightCapCount = 10,
-        flightCapPeriod = 86400,
-    )
 
     val contentRecommendationEntity = ContentRecommendationEntity(
         corpusItemId = "1111",
