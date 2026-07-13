e9a9cf940502e85005efeb732091b7b4503f4507
Rob Wu <rob@robwu.nl>
2026-07-13 23:28:47 +0000

    Bug 2054233 - Expand GV and A-C docs on nativeMessaging r=geckoview-reviewers,android-reviewers,owlish
    - Document message queuing behavior. See `mPendingMessages` in
      `geckoview/WebExtensionController.java` for its implementation.
      This behavior is significant because it tells how reliable a
      `sendNativeMessage` or `connectNative` call is from the top of
      a background script while an extension is being installed.
    
    - Document "geckoViewAddons" and "nativeMessagingFromContent"
      permissions and their requirements (enforced via
      `PRIVILEGED_PERMS_ANDROID_ONLY` in `Extension.sys.mjs`).
    
    - Document the risk of using content messaging, as well as the fact that
      the handler can receive from extension pages (added in bug 1554260).
    
    - Document the format of native app ID. The Firefox Source Docs lists
      an example with "browser", and it seems that several tests cargo cult
      copied it, instead of making up a different descriptive ID.
    
    - In various places, explain the relation between the GV API and
      extensions, so that readers do not have to dig through tons of layers
      and implementation details to figure out answers.
    
    Differential Revision: https://phabricator.services.mozilla.com/D311853

--- components/concept/engine/src/main/java/mozilla/components/concept/engine/webextension/WebExtension.kt
+++ components/concept/engine/src/main/java/mozilla/components/concept/engine/webextension/WebExtension.kt
@@ -27,6 +27,10 @@ abstract class WebExtension(
 ) {
     /**
      * Registers a [MessageHandler] for message events from background scripts.
+     * Messages received before a listener is registered are queued and
+     * dispatched after the message handler is registered. An extension can
+     * only send messages if it has the "geckoViewAddons" permission (only
+     * available to built-in or privileged extensions).
      *
      * @param name the name of the native "application". This can either be the
      * name of an application, web extension or a specific feature in case
@@ -41,7 +45,17 @@ abstract class WebExtension(
     abstract fun registerBackgroundMessageHandler(name: String, messageHandler: MessageHandler)
 
     /**
-     * Registers a [MessageHandler] for message events from content scripts.
+     * Registers a [MessageHandler] for message events from content scripts or
+     * extension pages. Messages received before a listener is registered are
+     * queued and dispatched after the message handler is registered. A content
+     * script can only send messages if it has the "geckoViewAddons" and
+     * "nativeMessagingFromContent" permissions (only available to built-in or
+     * privileged extensions). Messages can be sent from extension pages
+     * without the "nativeMessagingFromContent" permission.
+     *
+     * Warning: content scripts are hosted in untrusted web content processes,
+     * extension pages in a more privileged extension process (the same as
+     * background scripts handled by registerBackgroundMessageHandler).
      *
      * @param session the session to be observed / attach the message handler to.
      * @param name the name of the native "application". This can either be the
@@ -224,8 +238,18 @@ interface ActionHandler {
 }
 
 /**
- * A handler for all messaging related events, usable for both content and
- * background scripts.
+ * A handler for all messaging related events, usable for content scripts (and
+ * extension pages) and background scripts.
+ *
+ * To register a handler, use WebExtension.registerBackgroundMessageHandler or
+ * WebExtension.registerContentMessageHandler. The extension can only send
+ * messages if their manifest.json contains the "geckoViewAddons" permission,
+ * and in case of content scripts, the "nativeMessagingFromContent" permission.
+ * These permissions are only available to built-in (or privileged) extensions.
+ *
+ * See the documentation for the GeckoView API (that this API wraps) for more
+ * information on usage of the extension API and its requirements, at
+ * https://firefox-source-docs.mozilla.org/mobile/android/geckoview/consumer/web-extensions.html
  *
  * [Port]s are exposed to consumers (higher level components) because
  * how ports are used, how many there are and how messages map to it
@@ -267,7 +291,7 @@ interface MessageHandler {
      * @param message the received message, either be a primitive type
      * or a org.json.JSONObject.
      * @param source the session this message originated from if from a content
-     * script, otherwise null.
+     * script or extension page, otherwise null.
      * @return the response to be sent for this message, either a primitive
      * type or a org.json.JSONObject, null if no response should be sent.
      */
