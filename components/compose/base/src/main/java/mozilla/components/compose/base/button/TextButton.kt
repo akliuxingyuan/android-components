a2f07100d54c189d7c32a0ada29b0cb8db5475ed
Devota Aabel <daabel@mozilla.com>
2026-08-13 20:22:42 +0000

    Bug 2062780 - Update TextButton so that it can take a content block for icons. r=android-reviewers,007
    Differential Revision: https://phabricator.services.mozilla.com/D317955

--- components/compose/base/src/main/java/mozilla/components/compose/base/button/TextButton.kt
+++ components/compose/base/src/main/java/mozilla/components/compose/base/button/TextButton.kt
@@ -7,18 +7,26 @@ package mozilla.components.compose.base.button
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.RowScope
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.ButtonColors
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.LocalTextStyle
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.tooling.preview.PreviewLightDark
 import mozilla.components.compose.base.theme.AcornTheme
 import mozilla.components.compose.base.theme.acornPrivateColorScheme
 import mozilla.components.compose.base.theme.privateColorPalette
 import androidx.compose.material3.TextButton as M3TextButton
+import mozilla.components.ui.icons.R as iconsR
 
 /**
  * Text-only button.
@@ -55,6 +63,41 @@ fun TextButton(
     }
 }
 
+/**
+ * Text button displaying custom [content], for the cases where a label alone is not enough, such as
+ * a button pairing an icon with its label.
+ *
+ * @param onClick Invoked when the user clicks on the button.
+ * @param modifier [Modifier] Used to shape and position the underlying [androidx.compose.material3.TextButton].
+ * @param enabled Controls the enabled state of the button. When `false`, this button will not
+ * be clickable.
+ * @param colors The [ButtonColors] used to color the [TextButton]. The content color is provided to
+ * [content] as [androidx.compose.material3.LocalContentColor].
+ * @param border Optional [BorderStroke] to apply to the [TextButton].
+ * @param content The content to display inside the button, laid out in a [RowScope].
+ */
+@Composable
+fun TextButton(
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier,
+    enabled: Boolean = true,
+    colors: ButtonColors = ButtonDefaults.textButtonColors(),
+    border: BorderStroke? = null,
+    content: @Composable RowScope.() -> Unit,
+) {
+    M3TextButton(
+        onClick = onClick,
+        modifier = modifier,
+        enabled = enabled,
+        colors = colors,
+        border = border,
+    ) {
+        CompositionLocalProvider(LocalTextStyle provides AcornTheme.typography.button) {
+            content()
+        }
+    }
+}
+
 @Composable
 @PreviewLightDark
 private fun TextButtonPreview() {
@@ -70,6 +113,29 @@ private fun TextButtonPreview() {
                 onClick = {},
                 enabled = false,
             )
+        }
+    }
+}
+
+@Composable
+@PreviewLightDark
+private fun TextButtonWithContentPreview() {
+    AcornTheme {
+        Column(Modifier.background(MaterialTheme.colorScheme.surface)) {
+            TextButton(onClick = {}) {
+                Icon(
+                    painter = painterResource(iconsR.drawable.mozac_ic_chevron_down_16),
+                    contentDescription = null,
+                )
+
+                Spacer(modifier = Modifier.width(AcornTheme.layout.space.static100))
+
+                Text(
+                    text = "label",
+                    style = AcornTheme.typography.button,
+                    maxLines = 1,
+                )
+            }
         }
     }
 }
