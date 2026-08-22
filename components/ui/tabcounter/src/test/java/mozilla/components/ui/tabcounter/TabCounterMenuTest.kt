2d0139ca2e1e3b2e98363a000e5e5ef0e6b04cc7
Aaditya Dhingra <adhingra@mozilla.com>
2026-08-22 18:53:02 +0000

    Bug 2049696 - Add the missing MPL license header to Android test files r=android-reviewers,jonalmeida
    Differential Revision: https://phabricator.services.mozilla.com/D319337

--- components/ui/tabcounter/src/test/java/mozilla/components/ui/tabcounter/TabCounterMenuTest.kt
+++ components/ui/tabcounter/src/test/java/mozilla/components/ui/tabcounter/TabCounterMenuTest.kt
@@ -1,3 +1,7 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import mozilla.components.support.test.robolectric.testContext
 import mozilla.components.ui.tabcounter.TabCounterMenu
